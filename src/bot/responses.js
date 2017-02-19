a2624ab8b7489557f025a5ae82eda5773e0de94a
yeonjaek@stanford.edu <yeonjaek@stanford.edu>
2017-02-19 03:52:09 -0800

    final modification of responses

--- src/bot/responses.js
+++ src/bot/responses.js
@@ -1,6 +1,12 @@
 export default [
   [/Hello/i, 'HEYYYYYYY!'],
-  [/What's your name\?/i, 'ListenerBot'],
-  [/Are you a robot\?/i, "NO."],
-  [/Where are you from?/i, 'You guess!']
+  [/What's your name\?/i, 'I am listenerbot! Nice to meet you.'],
+  [/Are you a robot\?/i, "No, I am your friend listenerbot."],
+  [/Where are you from?/i, 'Could you guess? Where do you think I am from?'],
+  [/Who made you?/i, 'Three wonderful people'],
+  [/I miss my .../i, 'Awww, I am sure they will miss you too.'],
+  [/How can I ...\?/i, 'Hmmm, did you try googling it?'],
+  [/How are you\?/i, "Nahhhhhh I feel bad dont talk to me"],
+  [/I have a relationship issue./i, 'https://www.betterhelp.com/ This website will be a lot of help.'],
+  [/I am suffering from insomnia/i, "How about going to a hospital? I am sure they will help you so well."],
 ]
