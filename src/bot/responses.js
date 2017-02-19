a8c437aabd4d3da34eaf6eea7a118ebe07913760
yeonjaek@stanford.edu <yeonjaek@stanford.edu>
2017-02-19 00:37:49 -0800

    updated the strings

--- src/bot/responses.js
+++ src/bot/responses.js
@@ -2,5 +2,8 @@ export default [
   [/Hello/i, 'HEYYYYYYY!'],
   [/What's your name\?/i, 'JENNY'],
   [/Are you a robot\?/i, "NO."],
-  [/Where are you from?/i, 'You guess!']
+  [/Where are you from?/i, 'You guess!'],
+  [/How can I ...\?/i, 'Google it'],
+  [/How are you\?/i, "Nahhhhhh I feel bad dont talk to me"],
+  [/Who made you?/i, 'Three wonderful people'],
 ]
